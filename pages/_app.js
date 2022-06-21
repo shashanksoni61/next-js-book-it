269216a2f622905a6c62eb1dd2cd3eff2e462986
shashanksoni61 <shashanksoni61@gmail.com>
2022-06-21 19:07:36 +0530

    wrapped App in Redux wrapper & Handled 1st Redux Dispatch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import { wrapper } from "../store/index";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import colors from "colors";
 import Homepage from "@components/Homepage";
 import Layout from "@components/UI/Layout";
+import { getAllRooms } from "store/actions/room/room";
+
+import { wrapper } from "../store";
 
 export default function Home() {
   return (
@@ -8,3 +12,9 @@ export default function Home() {
     </Layout>
   );
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    await store.dispatch(getAllRooms());
+  }
+);
