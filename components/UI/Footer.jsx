7d59fba1e829e1f6c62469769b079378f796d333
shashanksoni61 <shashanksoni61@gmail.com>
2022-06-22 17:50:38 +0530

    Style Related Changes

--- components/UI/Footer.jsx
+++ components/UI/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-1">
       <p className="text-center mt-1">
-        Book IT - 2019-2021, All Rights Reserved
+        Book IT - 2021-2022, All Rights Reserved
       </p>
     </footer>
   );
