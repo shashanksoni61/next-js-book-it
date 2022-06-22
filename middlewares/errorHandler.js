b355b39208a44ad262ed2ade4a93754334b288ed
shashanksoni61 <shashanksoni61@gmail.com>
2022-06-22 14:20:37 +0530

    Added Middlewares  For Backend Code Refactoring

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,4 @@
+import colors from "colors";
 import { responseHandler } from "@utils/backend/responseHandler";
 import mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
@@ -6,4 +7,17 @@ export const objectIdErrorHandler = (req, res, next) => {
   const id = req.query.id;
   if (ObjectId.isValid(id)) next();
   else responseHandler(res, 400, false, null, "Invalid Id Or Bad Request");
+};
+
+export const errorMiddleware = (err, req, res, next) => {
+  console.log(
+    colors.bgMagenta({
+      error: {
+        error: err,
+        message: err.message,
+        stack: err.stack,
+      },
+    })
+  );
+  responseHandler(res, 400, false, null, err.message);
 };
